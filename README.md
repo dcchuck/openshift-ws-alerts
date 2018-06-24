8a32e06a2333588aba8f4750ef73f78e89ffae85
dcchuck <chuck.danielsson@gmail.com>
2018-06-24 17:25:47 -0400

    Basic websocket that connects to instance & publicly publishes new builds of app by name

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # OpenShift WS Alerts
 
 In this repo we build a simple websocket server that connects to an OpenShift instance and send alerts of completed builds.
+
+## Server Side Environment Variables
+
+* `TOKEN` Generate a token for a service account as documented [here](https://docs.openshift.com/container-platform/3.5/rest_api/index.html#rest-api-serviceaccount-tokens)
+* `NODE_TLS_REJECT_UNAUTHORIZED` This is not a production flag but required if you are not using valid ssl certs (i.e. self signed, etc.)
--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-// This demo is deployed using a self signed cert - do not use this line in production!
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
-const demoToken = process.env.TOKEN;
+const http = require('http');
 const WebSocket = require('ws');
 
-const webSocketUrl = `wss://129.146.147.219:8443/oapi/v1/watch/namespaces/my-test/builds?access_token=${demoToken}`
-const ws = new WebSocket(webSocketUrl);
+const demoToken = process.env.TOKEN;
 
-console.log(webSocketUrl);
+const host = '129.146.147.219:8443';
+const ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+const port = process.env.OPENSHIFT_NODEJS_PORT || 3000
 
-ws.on('message', function incoming(data) {
-    console.log(data);
-});
+const server = http.createServer();
+
+const wsServer = new WebSocket.Server({ server: server });
+
+server.listen(port, ipaddr, () => {
+    console.log('listening!')
+    const ws = new WebSocket(`wss://${host}/oapi/v1/watch/namespaces/my-test/builds?access_token=${demoToken}`, { origin: `https://${host}` });
+    wsServer.broadcast = function broadcast(data) {
+        wsServer.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
+    };
+    ws.onmessage = (message) => {
+        const messageData = JSON.parse(message.data);
+        const appName = messageData.object.metadata.labels.app;
+        if (messageData.object.status.phase === "Complete" && messageData.type === "MODIFIED") {
+            wsServer.broadcast(`New build complete - ${appName} New Version Available!`);
+        }
+    };
+})
