771d59906e599ad1ea91241927cfd0c529388869
dcchuck <chuck.danielsson@gmail.com>
2018-06-24 17:38:24 -0400

    Follow node example

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const WebSocket = require('ws');
 const demoToken = process.env.TOKEN;
 
 const host = '129.146.147.219:8443';
-const ipaddr = process.env.PORT || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-const port = process.env.IP || process.env.OPENSHIFT_NODEJS_PORT || 3000
+const ipaddr = process.env.PORT || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
+const port = process.env.IP || process.env.OPENSHIFT_NODEJS_PORT || 8080
 
 console.log(ipaddr);
 console.log(port);
@@ -17,6 +17,7 @@ const wsServer = new WebSocket.Server({ server: server });
 server.listen(port, ipaddr, () => {
     console.log('listening!')
     const ws = new WebSocket(`wss://${host}/oapi/v1/watch/namespaces/my-test/builds?access_token=${demoToken}`, { origin: `https://${host}` });
+    console.log(ws);
     wsServer.broadcast = function broadcast(data) {
         wsServer.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
@@ -25,6 +26,7 @@ server.listen(port, ipaddr, () => {
         });
     };
     ws.onmessage = (message) => {
+        console.log('message received!')
         const messageData = JSON.parse(message.data);
         const appName = messageData.object.metadata.labels.app;
         if (messageData.object.status.phase === "Complete" && messageData.type === "MODIFIED") {
