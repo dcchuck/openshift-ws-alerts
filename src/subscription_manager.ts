775078f2bd692bf0396fe498b64e572a16ab0440
dcchuck <chuck.danielsson@gmail.com>
2018-07-14 23:52:43 -0400

    wip typescript subscription logic

--- src/subscription_manager.ts
+++ src/subscription_manager.ts
@@ -1,4 +1,4 @@
-import { ISubscriptionMessage, ISubscriptionMessagePayload } from '../server';
+import { ISubscriptionMessage } from '../server';
 
 export default class SubscriptionManager {
     public subscriptions: any[];
